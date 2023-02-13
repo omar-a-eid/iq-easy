6a36d6e9d08cb97ac6b4ac493ab16da71c4e67f8
omar-a-eid <omerx9988@gmail.com>
2023-02-13 16:33:41 +0200

    env

--- util/connectDB.js
+++ util/connectDB.js
@@ -2,5 +2,12 @@ import mongoose from "mongoose";
 
 export const connectMongo = async () => {
   mongoose.set("strictQuery", false);
-  mongoose.connect(process.env.MONGO_URI);
+  if (!process.env.MONGO_URI) {
+    console.log("Env isn't provided");
+    mongoose.connect(
+      "mongodb+srv://omar:fF04J6Mi7kB9WjC6@cluster0.gsxtcow.mongodb.net/?retryWrites=true&w=majority"
+    );
+  } else {
+    mongoose.connect(process.env.MONGO_URI);
+  }
 };
